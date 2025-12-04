115bdc1bd322d1fc0827180798c3a9e482455cef
amirjonoqilov786 <amirjonqoilov786@gmail.com>
2025-12-04 20:04:43 +0500

    Fix: Add missing os import in settings.py

--- FSWD/settings.py
+++ FSWD/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'FSWD.wsgi.application'
 
 
 import dj_database_url
-import os
+
 
 DATABASES = {
     'default': dj_database_url.parse(
