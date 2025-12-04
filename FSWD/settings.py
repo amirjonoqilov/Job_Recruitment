9a570811d6975285d52e81ddb506fdf58abdf4cc
amirjonoqilov786 <amirjonqoilov786@gmail.com>
2025-12-04 19:17:08 +0500

    Prepare for Render deployment: add Procfile, update settings.py, requirements.txt

--- FSWD/settings.py
+++ FSWD/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tta4my0-4+zd-8!ck^#it^acex+08!m$0)1plq&@06e+0ll_@y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -72,7 +73,7 @@ TEMPLATES = [
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 WSGI_APPLICATION = 'FSWD.wsgi.application'
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn FSWD.wsgi:application
